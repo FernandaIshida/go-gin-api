d27616812810aa2cb25942b32cc2b4063c9f17ea
Fernanda Ishida <fer.ishida@gmail.com>
2026-03-19 19:01:38 -0300

    feat: add new endpoints and controller logic for student management

--- controllers/controller.go
+++ controllers/controller.go
@@ -1,10 +1,87 @@
 package controllers
 
 import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/FernandaIshida/go-gin-api.git/database"
+	"github.com/FernandaIshida/go-gin-api.git/messaging"
 	"github.com/FernandaIshida/go-gin-api.git/models"
 	"github.com/gin-gonic/gin"
 )
 
 func ShowAllStudents(c *gin.Context) {
-	c.JSON(200, models.Students)
+	var students []models.Student
+	result := database.DB.Find(&students)
+
+	if result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, students)
+}
+
+func CreateNewStudent(c *gin.Context) {
+	var student models.Student
+
+	if err := c.ShouldBindJSON(&student); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	database.DB.Create(&student)
+
+	jsonData, _ := json.Marshal(student)
+	messaging.PublishMessage("students_queue", string(jsonData))
+
+	c.JSON(http.StatusOK, student)
+}
+
+func ShowStudentByID(c *gin.Context) {
+	var student models.Student
+	id := c.Params.ByName("id")
+	database.DB.First(&student, id)
+
+	if student.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Student not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, student)
+}
+
+func DeleteStudentByID(c *gin.Context) {
+	var student models.Student
+	id := c.Params.ByName("id")
+	database.DB.Delete(&student, id)
+	c.JSON(http.StatusOK, gin.H{"message": "Student deleted"})
+}
+
+func UpdateStudentByID(c *gin.Context) {
+	var student models.Student
+	id := c.Params.ByName("id")
+	database.DB.First(&student, id)
+
+	if err := c.ShouldBindJSON(&student); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	database.DB.Model(&student).UpdateColumns(student)
+	c.JSON(http.StatusOK, student)
+
+}
+
+func SearchStudentByCPF(c *gin.Context) {
+	var student models.Student
+	cpf := c.Param("cpf")
+	database.DB.Where(&models.Student{CPF: cpf}).First(&student)
+
+	if student.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Student not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, student)
 }
--- routes/routes.go
+++ routes/routes.go
@@ -8,5 +8,10 @@ import (
 func HandleRequests() {
 	r := gin.Default()
 	r.GET("/students", controllers.ShowAllStudents)
+	r.GET("/students/:id", controllers.ShowStudentByID)
+	r.GET("/students/cpf/:cpf", controllers.SearchStudentByCPF)
+	r.POST("/students", controllers.CreateNewStudent)
+	r.DELETE("students/:id", controllers.DeleteStudentByID)
+	r.PATCH("/students/:id", controllers.UpdateStudentByID)
 	r.Run()
 }
